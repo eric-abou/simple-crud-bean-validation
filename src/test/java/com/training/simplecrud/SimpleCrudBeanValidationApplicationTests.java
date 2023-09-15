66c0a0dc059a622071530d2e50bdbe36330502bb
Eric ABOU <eric.abou@sndi.ci>
2023-09-15 22:03:32 +0000

    first commit

---
+++ src/test/java/com/training/simplecrud/SimpleCrudBeanValidationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.simplecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleCrudBeanValidationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
